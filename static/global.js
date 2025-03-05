315e77684c870362a31a4d92a6e2073317c08e26
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 17:27:03 -0500

    no way

--- static/global.js
+++ static/global.js
@@ -13,24 +13,24 @@ function $$ (selector, context = document) {
 // }
 
 let pages = [
-	{url: "./my-portfolio", title: "Home"},
-	{url: "./projects", title: "Projects"},
-	{url: "./contact", title: "Contact"},
-    {url: "./resume", title: "Resume"},
+	{url: ".", title: "Home"},
+	{url: "projects", title: "Projects"},
+	{url: "contact", title: "Contact"},
+    {url: "resume", title: "Resume"},
     {url: "https://github.mit.edu/pxliu", title: "Github"}
 ];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 for (let p of pages) {
 	let url = p.url;
 	let title = p.title;
 	// Create link and add it to nav
-    // if (!ARE_WE_HOME && !url.startsWith("http")) {
-    url = "../" + url;
+    // if (!url.startsWith("http")) {
+    // url = "../" + url;
     // }
 	let a = document.createElement("a");
     a.href = url;
