99ca40c24bd5d79028bdce0bc1c672fe3c015149
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 17:33:22 -0500

    idk anymore

--- static/global.js
+++ static/global.js
@@ -29,9 +29,9 @@ for (let p of pages) {
 	let url = p.url;
 	let title = p.title;
 	// Create link and add it to nav
-    // if (!url.startsWith("http")) {
-    // url = "../" + url;
-    // }
+    if (!url.startsWith("http")) {
+    url = "../" + url;
+    }
 	let a = document.createElement("a");
     a.href = url;
     a.textContent = title;
