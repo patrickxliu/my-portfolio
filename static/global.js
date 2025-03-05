be5bb248578893860eeb591bc1e85df401a942c0
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 16:45:25 -0500

    sdkv sadkv ksad

--- static/global.js
+++ static/global.js
@@ -13,10 +13,10 @@ function $$ (selector, context = document) {
 // }
 
 let pages = [
-	{url: "./", title: "Home"},
-	{url: "./projects", title: "Projects"},
-	{url: "./contact", title: "Contact"},
-    {url: "./resume", title: "Resume"},
+	{url: "./my-portfolio", title: "Home"},
+	{url: "./my-portfolio/projects", title: "Projects"},
+	{url: "./my-portfolio/contact", title: "Contact"},
+    {url: "./my-portfolio/resume", title: "Resume"},
     {url: "https://github.mit.edu/pxliu", title: "Github"}
 ];
 
