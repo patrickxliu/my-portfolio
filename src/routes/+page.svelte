72a2560ccba588c09f023a2e33fc30674b991598
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 18:31:10 -0500

    fix formatting again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,8 @@
 </p>
 <img src="images/headshot.JPG" height="400" alt="A professional headshot of me in a suit and glasses.">
 
+<h2>Latest Projects</h2>
 <div class="projects">
-    <h2>Latest Projects</h2>
-
     {#each projects.slice(0, 3) as p}
         <Project data={p} hLevel="3"/>
     {/each}
