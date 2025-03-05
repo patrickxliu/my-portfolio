2d322cb2a93af5c7d8da88616cb56d5f13005ad5
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 18:22:54 -0500

    updated home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+</script>
+
 <h1 class="home">
     Patrick Liu
 </h1>
 <p>
     Senior studying computer science at MIT. Ask me about fencing, basketball, or video games!
 </p>
-<img src="images/headshot.JPG" height="400" alt="A professional headshot of me in a suit and glasses.">
+
+<div class="projects">
+    <h2>Latest Projects</h2>
+
+    <img src="images/headshot.JPG" height="400" alt="A professional headshot of me in a suit and glasses.">
+    {#each projects.slice(0, 3) as p}
+        <Project data={p} />
+    {/each}
+</div>
