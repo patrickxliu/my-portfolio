7c942cc5479175a012705ba890a285ce43d7fde0
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 16:40:28 -0500

    its fixed????

--- static/global.js
+++ static/global.js
@@ -23,22 +23,22 @@ let pages = [
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-// const ARE_WE_HOME = document.documentElement.classList.contains("home");
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 for (let p of pages) {
 	let url = p.url;
 	let title = p.title;
 	// Create link and add it to nav
-    // if (!ARE_WE_HOME && !url.startsWith("http")) {
-    //     url = "../" + url;
-    // }
+    if (!ARE_WE_HOME && !url.startsWith("http")) {
+        url = "../" + url;
+    }
 	let a = document.createElement("a");
     a.href = url;
     a.textContent = title;
 
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
+    // if (a.host === location.host && a.pathname === location.pathname) {
+    //     a.classList.add("current");
+    // }
 
     if (a.host !== location.host){
         a.target = "_blank";
