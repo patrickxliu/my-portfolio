01f8c737f5582f7b95ffd103ccd892c38f914f53
pxliu <patrickxinyiliu@gmail.com>
2025-03-05 15:51:32 -0500

    removed home page tracking

--- static/global.js
+++ static/global.js
@@ -23,15 +23,15 @@ let pages = [
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 for (let p of pages) {
 	let url = p.url;
 	let title = p.title;
 	// Create link and add it to nav
-    if (!ARE_WE_HOME && !url.startsWith("http")) {
-        url = "../" + url;
-    }
+    // if (!ARE_WE_HOME && !url.startsWith("http")) {
+    //     url = "../" + url;
+    // }
 	let a = document.createElement("a");
     a.href = url;
     a.textContent = title;
