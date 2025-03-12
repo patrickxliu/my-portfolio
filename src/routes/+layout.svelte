3d461f70bc5117db6cc1cfe9e14c4afc477e6587
pxliu <patrickxinyiliu@gmail.com>
2025-03-12 12:10:07 -0400

    new nav bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+YOLO <slot /> 
+
+<script>
+    let pages = [
+  { url: "./", title: "Home" },
+  { url: "./projects", title: "Projects" },
+  { url: "./contact", title: "Contact" },
+  { url: "./resume", title: "Resume"},
+  { url: "https://github.mit.edu/pxliu", title: "Github"},
+  // add the rest of your pages here
+];
+</script>
+
+<nav>
+    {#each pages as p}
+    <a href="{p.url}">{p.title}</a>
+    {/each}
+</nav>
--- static/global.js
+++ static/global.js
@@ -12,37 +12,37 @@ function $$ (selector, context = document) {
 // 	currentLink.classList.add("current");
 // }
 
-let pages = [
-	{url: "https://patrickxliu.github.io/my-portfolio/", title: "Home"},
-	{url: "https://patrickxliu.github.io/my-portfolio/projects", title: "Projects"},
-	{url: "https://patrickxliu.github.io/my-portfolio/contact", title: "Contact"},
-    {url: "https://patrickxliu.github.io/my-portfolio/resume", title: "Resume"},
-    {url: "https://github.mit.edu/pxliu", title: "Github"}
-];
+// let pages = [
+// 	{url: "https://patrickxliu.github.io/my-portfolio/", title: "Home"},
+// 	{url: "https://patrickxliu.github.io/my-portfolio/projects", title: "Projects"},
+// 	{url: "https://patrickxliu.github.io/my-portfolio/contact", title: "Contact"},
+//     {url: "https://patrickxliu.github.io/my-portfolio/resume", title: "Resume"},
+//     {url: "https://github.mit.edu/pxliu", title: "Github"}
+// ];
 
-let nav = document.createElement("nav");
-document.body.prepend(nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
 
-// const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// // const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-for (let p of pages) {
-	let url = p.url;
-	let title = p.title;
+// for (let p of pages) {
+// 	let url = p.url;
+// 	let title = p.title;
 
-	let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
+// 	let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
 
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add("current");
+//     }
 
-    if (a.host !== location.host){
-        a.target = "_blank";
-    }
-    nav.append(a);
+//     if (a.host !== location.host){
+//         a.target = "_blank";
+//     }
+//     nav.append(a);
 
-}
+// }
 // console.log(location.pathname)
 // console.log(nav)
 
