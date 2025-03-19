2875c8ffa4eb710b2cdb2bf8d170e4fc3360c81e
pxliu <patrickxinyiliu@gmail.com>
2025-03-19 18:01:40 -0400

    add real data

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -3,14 +3,7 @@
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 
-    let data = [
-        { value: 1, label: "apples" },
-        { value: 2, label: "oranges" },
-        { value: 3, label: "mangos" },
-        { value: 4, label: "pears" },
-        { value: 5, label: "limes" },
-        { value: 5, label: "cherries" }
-    ];
+    export let data = [];
     let sliceGenerator = d3.pie().value(d => d.value);
     let arcData = sliceGenerator(data);
     let arcs = arcData.map(d => arcGenerator(d));
