f2410d78dbb37330e656b685ebaaec5531adeb01
pxliu <patrickxinyiliu@gmail.com>
2025-03-19 17:09:01 -0400

    add arc

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,4 +1,14 @@
-“Hello from Pie.svelte”
+<script>
+    import * as d3 from 'd3';
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let arc = arcGenerator({
+	startAngle: 0,
+	endAngle: 2 * Math.PI
+});
+
+
+</script>
 
 <svg viewBox="-50 -50 100 100">
+    <path d={arc} fill="red" />
 </svg>
