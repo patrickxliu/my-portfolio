42471527912df8a46bd78960a3b7da2124bfbc56
pxliu <patrickxinyiliu@gmail.com>
2025-03-19 17:18:50 -0400

    add legend

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -3,8 +3,15 @@
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 
-    let data = [1, 2, 3, 4, 5, 5];
-    let sliceGenerator = d3.pie();
+    let data = [
+        { value: 1, label: "apples" },
+        { value: 2, label: "oranges" },
+        { value: 3, label: "mangos" },
+        { value: 4, label: "pears" },
+        { value: 5, label: "limes" },
+        { value: 5, label: "cherries" }
+    ];
+    let sliceGenerator = d3.pie().value(d => d.value);
     let arcData = sliceGenerator(data);
     let arcs = arcData.map(d => arcGenerator(d));
 
@@ -17,6 +24,14 @@
         <path d={ arc } fill={ colors(index) } />
     {/each}
 </svg>
+<ul class="legend">
+	{#each data as d, index}
+		<li style="--color: { colors(index) }">
+			<span class="swatch"></span>
+			{d.label} <em>({d.value})</em>
+		</li>
+	{/each}
+</ul>
 
 <style>
     svg {
