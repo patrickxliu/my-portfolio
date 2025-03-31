b3ee19ba719083c402b72c52d088165bde2aeb94
pxliu <patrickxinyiliu@gmail.com>
2025-03-31 14:34:46 -0400

    log commits

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -6,8 +6,40 @@
   let data = [];
 
   onMount(async () => {
-    data = await d3.csv("images/loc.csv");
+    data = await d3.csv("images/loc.csv", row => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime)
+    }));
   });
+
+  let commits = [];
+  commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+    let first = lines[0];
+    let {author, date, time, timezone, datetime} = first;
+    let ret = {
+      id: commit,
+      url: "https://github.com/patrickxliu/my-portfolio/commit/" + commit,
+      author, date, time, timezone, datetime,
+      hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+      totalLines: lines.length
+    };
+
+    // Like ret.lines = lines
+    // but non-enumerable so it doesn’t show up in JSON.stringify
+    Object.defineProperty(ret, "lines", {
+      value: lines,
+      configurable: true,
+      writable: true,
+      enumerable: false,
+    });
+
+    return ret;
+  });
+  console.log(commits);
 </script>
 
 <svelte:head>
@@ -21,4 +53,7 @@
     Hello World!
 </p>
 
-<p>Total lines of code: {data.length}</p>
+<dl class="stats">
+	<dt>Total <abbr title="Lines of code">LOC</abbr></dt>
+	<dd>{data.length}</dd>
+</dl>
