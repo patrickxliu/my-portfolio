944c8f35041e922258c9c335cea77cb6efac6e82
pxliu <patrickxinyiliu@gmail.com>
2025-04-15 03:49:22 -0400

    revert

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -10,7 +10,6 @@
   } from '@floating-ui/dom';
 
   import Bar from '$lib/Bar.svelte';
-  import Scatterplot from "./Scatterplot.svelte";
 
   let data = [];
   let commits = [];
@@ -81,7 +80,66 @@
   $: filteredCommits = commits.filter(commit => commit.datetime <= commitMaxTime)
   $: filteredLines = data.filter(line => line.datetime <= commitMaxTime)
 
+  $: xScale = d3.scaleTime()
+                .domain([minDate, commitMaxTime])
+                .range([usableArea.left, usableArea.right])
+                .nice();
+
+  $: yScale = d3.scaleLinear()
+                .domain([24, 0])
+                .range([usableArea.bottom, usableArea.top]);
+
+  $: rScale = d3.scaleSqrt(d3.extent(filteredCommits, d=> d.totalLines), [2,25]);
+
+  let xAxis, yAxis, yAxisGridlines;
+
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+    d3.select(yAxisGridlines).call(
+      d3.axisLeft(yScale)
+        .tickFormat("")
+        .tickSize(-usableArea.width)
+    );
+  }
+
+  let hoveredIndex = -1;
+  $: hoveredCommit = filteredCommits[hoveredIndex] ?? hoveredCommit ?? {};
+
+  let cursor = {x: 0, y: 0};
+
+  let commitTooltip;
+  let tooltipPosition = {x: 0, y: 0};
+
   let clickedCommits = [];
+
+  async function dotInteraction (index, evt) {
+    let hoveredDot = evt.target;
+    if (evt.type === "mouseenter") {
+      hoveredIndex = index;
+      cursor = {x: evt.x, y: evt.y};
+      tooltipPosition = await computePosition(hoveredDot, commitTooltip, {
+        strategy: "fixed", // because we use position: fixed
+        middleware: [
+          offset(5), // spacing from tooltip to dot
+          autoPlacement() // see https://floating-ui.com/docs/autoplacement
+        ],
+      });        }
+    else if (evt.type === "mouseleave") {
+      hoveredIndex = -1
+    }
+    else if (evt.type === "click") {
+      let commit = commits[index]
+      if (!clickedCommits.includes(commit)) {
+        // Add the commit to the clickedCommits array
+        clickedCommits = [...clickedCommits, commit];
+      }
+      else {
+          // Remove the commit from the array
+          clickedCommits = clickedCommits.filter(c => c !== commit);
+      }
+    }
+  }
 
   $: allTypes = Array.from(new Set(data.map(d => d.type)));
   $: selectedLines = (clickedCommits.length > 0 ? clickedCommits : commits).flatMap(d => d.lines);
@@ -120,7 +178,52 @@
 </dl>
 
 <h3>Commits by time of day</h3>
-<Scatterplot filteredCommits={filteredCommits} bind:clickedCommits={clickedCommits} />
+<svg viewBox="0 0 {width} {height}">
+	<!-- scatterplot will go here -->
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+  <g class="dots">
+    {#each filteredCommits as commit, index (commit.id) }
+      <circle
+        class:selected={ clickedCommits.includes(commit) }
+        on:click={ evt => dotInteraction(index, evt) }
+        on:mouseenter={evt => dotInteraction(index, evt)}
+        on:mouseleave={evt => dotInteraction(index, evt)}
+        on:mouseenter={evt => {
+          hoveredIndex = index;
+          cursor = {x: evt.x, y: evt.y};
+        }}
+        on:mouseenter={evt => hoveredIndex = index}
+        on:mouseleave={evt => hoveredIndex = -1}
+        cx={ xScale(commit.datetime) }
+        cy={ yScale(commit.hourFrac) }
+        r={ rScale(commit.totalLines) }
+        fill="steelblue"
+        fill-opacity = ".8"
+      />
+    {/each}
+  </g>
+</svg>
+<Bar data={languageBreakdown} width={width} />
+
+<dl class="info tooltip" hidden={hoveredIndex === -1} style="top: {tooltipPosition.y}px; left: {tooltipPosition.x}px" bind:this={commitTooltip}>
+    <dt class="info">COMMIT</dt>
+    <dd class="info"><a href="{ hoveredCommit.url }" target="_blank">{ hoveredCommit.id }</a></dd>
+  
+    <dt class="info">DATE</dt>
+    <dd class="info">{ hoveredCommit.datetime?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+
+    <dt class="info">TIME</dt>
+    <dd class="info">{ hoveredCommit.time?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+
+    <dt class="info">AUTHOR</dt>
+    <dd class="info">{ hoveredCommit.author?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+
+    <dt class="info">LINES</dt>
+    <dd class="info">{ hoveredCommit.totalLines?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+    <!-- Add: Time, author, lines edited -->
+  </dl>
 
 
 
@@ -139,6 +242,67 @@
       text-align: left;
       font-size: x-large;
       font-weight: lighter;
+  }
+  svg {
+		overflow: visible;
+	}
+  .gridlines {
+    stroke-opacity: .2;
+  }
+
+  dl.info{
+    display: grid;
+    grid-template-columns: 3em 12em;
+    grid-template-rows: 1em,1em;
+    text-decoration: none;
+    margin: 0%;
+    padding: 1em;
+    border-radius: 5px;
+    border-style: solid;
+    border-color: gray;
+    background-color: oklch(100% 0% 0 / 80%);
+    backdrop-filter: blur(5px);
+
+    transition-duration: 500ms;
+    transition-property: opacity, visibility;
+
+    &[hidden]:not(:hover, :focus-within) {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  .tooltip{
+    position: fixed;
+    top: 1em;
+    left: 1em;
+  }
+  circle {
+    transition: 200ms;
+    transform-origin: center;
+    transform-box: fill-box;
+
+    &:hover {
+      transform: scale(1.5);
+    }
+    @starting-style {
+      r: 0;
+    }
+  }
+
+  dt.info{
+      text-align: right;
+      font-size:x-small;
+      font-weight:lighter;
+  }
+  dd.info{
+      text-align: left;
+      font-size: x-small;
+      font-weight: normal;
+
+  }
+  .selected {
+    fill: var(--color-accent);
   }
   .slider {
     width: 100vw;
